bda21a812d6f2144dd6055b912d72ba04edf9a2e
Will Kochanski <wspkochanski@gmail.com>
2016-08-08 15:02:45 -0400

    Added xmobar config, git ignores compiled xmonad files

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -1,13 +1,19 @@
 import XMonad
 import XMonad.Util.EZConfig
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
 
 term = "gnome-terminal"
 browser = "google-chrome"
 
-main = xmonad $ defaultConfig {
+main = xmonad =<< (xmobar $ defaultConfig {
     terminal    = term,
     borderWidth = 2,
-    startupHook = startup
+    startupHook = startup,
+    -- make sure xmobar isn't hidden
+    manageHook = manageDocks <+> manageHook defaultConfig,
+    layoutHook = avoidStruts $ layoutHook defaultConfig,
+    handleEventHook = docksEventHook <+> handleEventHook defaultConfig
 } `additionalKeysP` [ 
     ("C-<Return>",              spawn term),
     ("C-M-<Return>",            spawn browser),
@@ -16,13 +22,13 @@ main = xmonad $ defaultConfig {
     ("<XF86AudioRaiseVolume>",  spawn "amixer -D pulse sset Master 5%+"),
     ("<XF86AudioLowerVolume>",  spawn "amixer -D pulse sset Master 5%-"),
     ("<XF86AudioMute>",         spawn "amixer -D pulse sset Master toggle"),
-    ("M4-<F5>",   spawn "xbacklight -inc 10"),
-    ("M4-<F6>",   spawn "xbacklight -dec 10")]
+    ("M4-<F5>",   spawn "xbacklight -dec 10"),
+    ("M4-<F6>",   spawn "xbacklight -inc 10")])
 
 
 startup :: X ()
 startup = do
-    spawn "nm-applet"
+    -- spawn "nm-applet"
     spawn "setxkbmap -option ctrl:nocaps"
     spawn "xcape -e 'Control_L=Escape"
 
